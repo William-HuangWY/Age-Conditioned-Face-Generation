a5050273daf09c611b9b41725a3a712191a81ec5
William Huang <148852245+William-HuangWY@users.noreply.github.com>
2026-03-01 00:14:58 -0800

    minor fix on folder name

--- download_data.py
+++ download_data.py
@@ -3,7 +3,7 @@ import shutil
 import os
 
 path = kagglehub.dataset_download("frabbisw/facial-age")
-target_dir = os.getcwd() + "/dataset"
+target_dir = os.getcwd() + "/datasets"
 os.makedirs(target_dir, exist_ok=True)
 shutil.copytree(path, target_dir, dirs_exist_ok=True)
 shutil.rmtree(path)
